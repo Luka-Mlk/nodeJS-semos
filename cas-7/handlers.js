e045ad6de4542e7a7afaf36072e1084f1fe74438
Luka-Mlk <Luka-Mlk@users.noreply.github.com>
2023-01-25 02:42:06 +0100

    c7/8

--- cas-7/handlers.js
+++ cas-7/handlers.js
@@ -23,7 +23,30 @@ const calculator = (req, res) => {
   res.send(result.toString());
   res.send("");
 };
+// calculator2 handler funckija
+const calculator2 = (req, res) => {
+  let result;
+  switch (req.params.operation) {
+    case "sobiranje":
+      result = Number(req.query.a) + Number(req.query.b);
+      break;
+    case "odzemanje":
+      result = Number(req.query.a) - Number(req.query.b);
+      break;
+    case "mnozhenje":
+      result = Number(req.query.a) * Number(req.query.b);
+      break;
+    case "delenje":
+      result = Number(req.query.a) / Number(req.query.b);
+      break;
+    default:
+      result = 0;
+  }
+  res.send(result.toString());
+};
+
 module.exports = {
   home,
   calculator,
+  calculator2,
 };
